9964adbd993b814602526ff2761a94dcc3956049
MKy-GH <mario.elkhoury@bluewin.ch>
2021-05-01 18:06:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function initClient() {
         discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
         scope: "https://www.googleapis.com/auth/calendar.events.readonly",
         uxMode: 'redirect',
-        redirectUri: "https://mky-gh.github.io/CoolDashBoard/"
+        redirectUri: "https://mky-gh.github.io/CoolDashBoard"
     }).then(function () {
         GoogleAuth = gapi.auth2.getAuthInstance();
 
